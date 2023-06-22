f9169469d0545fe37fbb22c03dca58e867a8aa0e
Susan Gomez <susangomez@Susans-MBP.attlocal.net>
2023-06-22 19:42:33 -0400

    Added a template string with a section tag in dom.js

--- dom.js
+++ dom.js
@@ -3,3 +3,8 @@ const main = document.querySelector('main');
 const joke1 = ("What did the janitor say when he jumped out of the closet? Supplies!");
 const joke2 = ("What do you call a factory that makes okay products? A satisfactory.");
 const joke3 = ("How does the moon cut his hair? Eclipse it.");
+
+const template = `
+    <section>
+    </section>
+`
