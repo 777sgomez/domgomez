6c2791e4e2211e9bb4ec5df3e976a89dae15ca98
Susan Gomez <susangomez@Susans-MBP.attlocal.net>
2023-06-22 19:40:58 -0400

    Added a variable for the third joke in dom.js

--- dom.js
+++ dom.js
@@ -2,3 +2,4 @@ const main = document.querySelector('main');
 
 const joke1 = ("What did the janitor say when he jumped out of the closet? Supplies!");
 const joke2 = ("What do you call a factory that makes okay products? A satisfactory.");
+const joke3 = ("How does the moon cut his hair? Eclipse it.");
